fb75142b99b60693f933bd3ae0ed9c3c52d62f11
mattias <mattias.stromdahl@gmail.com>
2020-11-02 15:23:35 +0100

    changed node class

--- src/com/company/LinkedList/LinkedList.java
+++ src/com/company/LinkedList/LinkedList.java
@@ -16,7 +16,7 @@ public class LinkedList<E> {
     }
 
     public E get(int i) {
-            return head.get(i).value;
+        return head.get(i).value;
     }
 
     public int size() {
@@ -30,51 +30,51 @@ public class LinkedList<E> {
     public boolean empty() {
         return head == null;
     }
-}
 
-class Node<E> {
-    Node<E> next;
-    E value;
+    class Node<E> {
+        Node<E> next;
+        E value;
 
-    Node(E value) {
-        this.value = value;
-    }
-
-    Node(E value, Node nextElement) {
-        this.value = value;
-        this.next = nextElement;
-    }
-
-    protected void add(E value) {
-        if (this.next == null) {
-            this.next = new Node<>(value);
-        } else {
-            this.next.add(value);
-        }
-    }
-
-    protected void add(int i, E value) {
-        if (i > 0) {
-            this.next.add(i - 1, value);
-        } else {
-            this.next = new Node<E>(this.value, this.next);
+        Node(E value) {
             this.value = value;
         }
-    }
 
-    protected Node<E> get(int i) {
-        return (i > 0) ? this.next.get(i - 1) : this;
-    }
-
-    protected void remove(int index) {
-        if (index > 1) {
-            next.remove(index - 1);
-        } else {
-            this.next = next.next;
+        Node(E value, Node nextElement) {
+            this.value = value;
+            this.next = nextElement;
         }
-    }
 
-    protected int size(int i) {
-        return next == null ? i : next.size(i + 1);
+        protected void add(E value) {
+            if (this.next == null) {
+                this.next = new Node<>(value);
+            } else {
+                this.next.add(value);
+            }
+        }
+
+        protected void add(int i, E value) {
+            if (i > 0) {
+                this.next.add(i - 1, value);
+            } else {
+                this.next = new Node<E>(this.value, this.next);
+                this.value = value;
+            }
+        }
+
+        protected Node<E> get(int i) {
+            return (i > 0) ? this.next.get(i - 1) : this;
+        }
+
+        protected void remove(int index) {
+            if (index > 1) {
+                next.remove(index - 1);
+            } else {
+                this.next = next.next;
+            }
+        }
+
+        protected int size(int i) {
+            return next == null ? i : next.size(i + 1);
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,7 @@ package com.company;
 import com.company.LinkedList.LinkedList;
 
 public class Main {
-
-
     public static void main(String[] args) {
-        // write your code here
         LinkedList<Double> linkedList = new LinkedList();
         linkedList.add(1d);
     }
