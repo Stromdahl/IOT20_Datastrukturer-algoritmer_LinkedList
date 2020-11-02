aba24c0235c6460e5891e10692c718408f575936
mattias <mattias.stromdahl@gmail.com>
2020-11-02 15:18:42 +0100

    name change

--- src/com/company/LinkedList/LinkedListTest.java
+++ src/com/company/LinkedList/LinkedListTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AwesomeLinkedListTest {
+public class LinkedListTest {
 
     @Test
     @DisplayName("Test add a element to list")
