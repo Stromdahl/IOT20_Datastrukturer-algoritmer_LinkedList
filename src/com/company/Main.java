abbe6f55e84c17aad24c84d0f6cf7ec5d262192d
DESKTOP-MG1KFQA\Besim <besim.musliu@student.kyh.se>
2020-11-04 11:27:00 +0100

    Added class Queue, added methods as well, enqueue, dequeue, peek, size. Added tests for Class Queue

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,7 @@ import com.company.LinkedList.Stack;
 public class Main {
     public static void main(String[] args) {
 
-        java.util.LinkedList<Integer> javaList = new java.util.LinkedList<>();
-        javaList.add(0,1);
-        javaList.add(1,1);
-        javaList.add(2,1);
-        javaList.add(4,1);
+
+
     }
 }
